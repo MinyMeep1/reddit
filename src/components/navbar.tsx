dd4379626537d5f5060e1b4f1a2a4625e4d840a8
Adis Tucakovic <tucakovicsevko@gmail.com>
2024-06-27 02:08:27 -0400

    Initializing Project Files

---
+++ src/components/navbar.tsx
@@ -0,0 +1,13 @@
+
+
+export default function Navbar() {
+    return (
+        
+        <ul>
+            <h1>Reddit</h1>
+            <li>Timeline</li>
+            <li>Profile</li>
+        </ul>
+      
+    );
+  }
